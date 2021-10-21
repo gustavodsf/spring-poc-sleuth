030007a164a6d54dbb2035fadb52bd4d84482227
Gustavo Figueiredo <gustavo.figueiredo@nc0170.conductor.tecnologia>
2021-10-21 00:09:38 -0300

    feat add project

---
+++ slueth-test2/src/test/java/br/com/poc/slueth/sluethtest/SluethTestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.poc.slueth.sluethtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SluethTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ slueth-test3/src/main/java/br/com/poc/slueth/sluethtest/SluethTestApplication.java
@@ -0,0 +1,13 @@
+package br.com.poc.slueth.sluethtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SluethTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SluethTestApplication.class, args);
+	}
+
+}
